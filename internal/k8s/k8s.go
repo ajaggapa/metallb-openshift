metallb/metallb

Add --frrk8s-secret-passthrough for external frr-k8s
When frr-k8s runs in a separate namespace, allow passing BGP
secret references directly instead of resolving them to cleartext.
The user creates the secret in the frr-k8s namespace and metallb
forwards the reference as-is.

Co-Authored-By: Claude Opus 4.6 (1M context) <noreply@anthropic.com>
Signed-off-by: Ori Braunshtein <obraunsh@redhat.com>

internal/k8s/k8s.go
@@ -96,27 +96,28 @@ type Client struct {
 // Config specifies the configuration of the Kubernetes
 // client/watcher.
 type Config struct {
-	ProcessName            string
-	NodeName               string
-	PodName                string
-	MetricsPort            int
-	PprofBindAddress       string
-	HealthProbeBindAddress string
-	ReadEndpoints          bool
-	Logger                 log.Logger
-	Namespace              string
-	ValidateConfig         config.Validate
-	EnableWebhook          bool
-	TLSOpt                 func(*tls.Config)
-	DisableCertRotation    bool
-	MetricsCertDir         string
-	WebhookSecretName      string
-	CertDir                string
-	CertServiceName        string
-	LoadBalancerClass      string
-	WebhookWithHTTP2       bool
-	WithFRRK8s             bool
-	FRRK8sNamespace        string
+	ProcessName             string
+	NodeName                string
+	PodName                 string
+	MetricsPort             int
+	PprofBindAddress        string
+	HealthProbeBindAddress  string
+	ReadEndpoints           bool
+	Logger                  log.Logger
+	Namespace               string
+	ValidateConfig          config.Validate
+	EnableWebhook           bool
+	TLSOpt                  func(*tls.Config)
+	DisableCertRotation     bool
+	MetricsCertDir          string
+	WebhookSecretName       string
+	CertDir                 string
+	CertServiceName         string
+	LoadBalancerClass       string
+	WebhookWithHTTP2        bool
+	WithFRRK8s              bool
+	FRRK8sNamespace         string
+	FRRK8sSecretPassthrough bool
 	Listener
 	Layer2StatusChan    <-chan event.GenericEvent
 	Layer2StatusFetcher controllers.L2StatusFetcher
@@ -213,15 +214,16 @@ func New(cfg *Config) (*Client, error) {
 
 	if cfg.ConfigChanged != nil {
 		if err = (&controllers.ConfigReconciler{
-			Client:          mgr.GetClient(),
-			ConfigStateName: configStateName,
-			Logger:          cfg.Logger,
-			Scheme:          mgr.GetScheme(),
-			Namespace:       cfg.Namespace,
-			ValidateConfig:  cfg.ValidateConfig,
-			Handler:         cfg.ConfigHandler,
-			ForceReload:     reload,
-			NodeName:        cfg.NodeName,
+			Client:                  mgr.GetClient(),
+			ConfigStateName:         configStateName,
+			Logger:                  cfg.Logger,
+			Scheme:                  mgr.GetScheme(),
+			Namespace:               cfg.Namespace,
+			ValidateConfig:          cfg.ValidateConfig,
+			Handler:                 cfg.ConfigHandler,
+			ForceReload:             reload,
+			NodeName:                cfg.NodeName,
+			FRRK8sSecretPassthrough: cfg.FRRK8sSecretPassthrough,
 		}).SetupWithManager(mgr); err != nil {
 			level.Error(c.logger).Log("error", err, "unable to create controller", "config")
 			return nil, errors.Join(err, errors.New("unable to create controller for config"))

ajaggapa/metallb-openshift

internal/k8s/k8s.go
@@ -97,27 +97,28 @@ type Client struct {
 // Config specifies the configuration of the Kubernetes
 // client/watcher.
 type Config struct {
-	ProcessName         string
-	NodeName            string
-	PodName             string
-	MetricsHost         string
-	MetricsPort         int
-	EnablePprof         bool
-	ReadEndpoints       bool
-	Logger              log.Logger
-	Namespace           string
-	ValidateConfig      config.Validate
-	EnableWebhook       bool
-	WebHookMinVersion   uint16
-	WebHookCipherSuites []uint16
-	DisableCertRotation bool
-	WebhookSecretName   string
-	CertDir             string
-	CertServiceName     string
-	LoadBalancerClass   string
-	WebhookWithHTTP2    bool
-	WithFRRK8s          bool
-	FRRK8sNamespace     string
+	ProcessName             string
+	NodeName                string
+	PodName                 string
+	MetricsHost             string
+	MetricsPort             int
+	EnablePprof             bool
+	ReadEndpoints           bool
+	Logger                  log.Logger
+	Namespace               string
+	ValidateConfig          config.Validate
+	EnableWebhook           bool
+	WebHookMinVersion       uint16
+	WebHookCipherSuites     []uint16
+	DisableCertRotation     bool
+	WebhookSecretName       string
+	CertDir                 string
+	CertServiceName         string
+	LoadBalancerClass       string
+	WebhookWithHTTP2        bool
+	WithFRRK8s              bool
+	FRRK8sNamespace         string
+	FRRK8sSecretPassthrough bool
 	Listener
 	Layer2StatusChan    <-chan event.GenericEvent
 	Layer2StatusFetcher controllers.L2StatusFetcher
@@ -200,14 +201,15 @@ func New(cfg *Config) (*Client, error) {
 
 	if cfg.ConfigChanged != nil {
 		if err = (&controllers.ConfigReconciler{
-			Client:          mgr.GetClient(),
-			ConfigStateName: configStateName,
-			Logger:          cfg.Logger,
-			Scheme:          mgr.GetScheme(),
-			Namespace:       cfg.Namespace,
-			ValidateConfig:  cfg.ValidateConfig,
-			Handler:         cfg.ConfigHandler,
-			ForceReload:     reload,
+			Client:                  mgr.GetClient(),
+			ConfigStateName:         configStateName,
+			Logger:                  cfg.Logger,
+			Scheme:                  mgr.GetScheme(),
+			Namespace:               cfg.Namespace,
+			ValidateConfig:          cfg.ValidateConfig,
+			Handler:                 cfg.ConfigHandler,
+			ForceReload:             reload,
+			FRRK8sSecretPassthrough: cfg.FRRK8sSecretPassthrough,
 		}).SetupWithManager(mgr); err != nil {
 			level.Error(c.logger).Log("error", err, "unable to create controller", "config")
 			return nil, errors.Join(err, errors.New("unable to create controller for config"))